a169b7fcf1eb15467ec97a6a7f8cbdb1caeed10a
Aditya1or0 <adityapandit264@gmail.com>
2025-08-15 22:59:52 +0530

    Feat: added middleware to get user token and give their booking verfiying via middleware

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -3,10 +3,11 @@ import {
   createBooking,
   getBookings,
 } from "../controllers/bookingController.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const bookingRouter = express.Router();
 
-bookingRouter.post("/", createBooking);
-bookingRouter.get("/", getBookings);
+bookingRouter.post("/", authMiddleware, createBooking);
+bookingRouter.get("/", authMiddleware, getBookings);
 
 export default bookingRouter;
